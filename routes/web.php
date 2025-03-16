03d5e124211e7332f784b139217f0dacd6b3ff23
Cristinarte <cristinarh.nrh@gmail.com>
2025-03-16 18:34:19 +0100

    Eliminar ruta temporal de almacenamiento

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,3 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-// Ruta temporal para arreglar el acceso a las imágenes en Render
-Route::get('/fix-storage', function () {
-    Artisan::call('storage:link');
-    Artisan::call('config:clear');
-    Artisan::call('cache:clear');
-    Artisan::call('route:clear');
-    Artisan::call('view:clear');
-    return "Storage linked and cache cleared!";
-});
