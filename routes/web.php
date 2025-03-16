c4f30fa10636334d52c6f7db480150f4593744b1
Cristinarte <cristinarh.nrh@gmail.com>
2025-03-16 18:00:38 +0100

    Arreglado acceso a archivos en almacenamiento y agregado ruta temporal

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-
+// Ruta temporal para arreglar el acceso a las imágenes en Render
+Route::get('/fix-storage', function () {
+    Artisan::call('storage:link');
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    return "Storage linked and cache cleared!";
+});
