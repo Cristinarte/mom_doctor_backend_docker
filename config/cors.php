203bcbe19f7e4ce32f44f4add01bb14b59e098b7
Cristinarte <cristinarh.nrh@gmail.com>
2025-03-17 09:01:01 +0100

    Modifico .env cors filesystem y doy permisos para probar mostrar imagen

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'https://momdoctor.web.app')],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://mom-doctor-frontend.onrender.com')],
 
     'allowed_origins_patterns' => [],
 
